6137f0c0ac8edf42297e22e4194b0ab329bc54c6
Szymon Gut <pog992@gmail.com>
2013-06-10 10:48:27 +0200

    fixed dynamic binding registration

--- client/src/main/java/pl/edu/agh/iosr/services/TenantServiceImpl.java
+++ client/src/main/java/pl/edu/agh/iosr/services/TenantServiceImpl.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.iosr.services;
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -8,14 +7,11 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,118 +21,109 @@ import pl.edu.agh.iosr.model.entity.StockCompany;
 import pl.edu.agh.iosr.model.entity.Tenant;
 import pl.edu.agh.iosr.msg.StockQuoteListener;
 
-import com.rabbitmq.client.Channel;
-
-
-
 @Service
 public class TenantServiceImpl implements TenantService {
-	
-	public static final String EXCHANGE_NAME = "stock-broker"; 
 
-    private EntityManager entityManager;
+	public static final String EXCHANGE_NAME = "stock-broker";
 
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }  
-    
-    @PersistenceContext
-    public void setEntityManager(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+	private EntityManager entityManager;
 
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
 
-    @Override
-    @Transactional
-    public List<Tenant> getAllTenants() {
-        Query query = getEntityManager().createQuery("from Tenant t");
-        List<Tenant> tenantList = (List<Tenant>) query.getResultList();
-        return  (tenantList != null && tenantList.size() > 0) ?  tenantList : null;
-    }
-
-    @Override
-    @Transactional
-    public List<Tenant> getAllNotSuperuserTenants() {
-        Query query = getEntityManager().createQuery("from Tenant t where t.name != :name");
-        query.setParameter("name", Tenant.SUPERUSER_TENANT_NAME);
-        List<Tenant> tenantList = (List<Tenant>) query.getResultList();
-        return  (tenantList != null && tenantList.size() > 0) ?  tenantList : null;
-    }
+	@PersistenceContext
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
 	@Override
-    @Transactional
-    public Tenant getTenantByName(String name) {
-        Query query = getEntityManager().createQuery(
-        		"from Tenant t where t.name =:name");
-        query.setParameter("name", name);
-        List<Tenant> tenantList = (List<Tenant>)query.getResultList() ;
-        return  (tenantList!= null && tenantList.size() > 0) ? (Tenant) (tenantList.get(0)) : null;
-    }
+	@Transactional
+	public List<Tenant> getAllTenants() {
+		Query query = getEntityManager().createQuery("from Tenant t");
+		List<Tenant> tenantList = (List<Tenant>) query.getResultList();
+		return (tenantList != null && tenantList.size() > 0) ? tenantList
+				: null;
+	}
+
+	@Override
+	@Transactional
+	public List<Tenant> getAllNotSuperuserTenants() {
+		Query query = getEntityManager().createQuery(
+				"from Tenant t where t.name != :name");
+		query.setParameter("name", Tenant.SUPERUSER_TENANT_NAME);
+		List<Tenant> tenantList = (List<Tenant>) query.getResultList();
+		return (tenantList != null && tenantList.size() > 0) ? tenantList
+				: null;
+	}
+
+	@Override
+	@Transactional
+	public Tenant getTenantByName(String name) {
+		Query query = getEntityManager().createQuery(
+				"from Tenant t where t.name =:name");
+		query.setParameter("name", name);
+		List<Tenant> tenantList = (List<Tenant>) query.getResultList();
+		return (tenantList != null && tenantList.size() > 0) ? (Tenant) (tenantList
+				.get(0)) : null;
+	}
 
 	@Override
 	public Tenant getTenantById(Long id) {
 		return getEntityManager().find(Tenant.class, id);
 	}
-	
+
 	@Override
-    @Transactional
-    public Tenant createTenant(Tenant tenant) {
-        getEntityManager().persist(tenant);
-        return tenant;
-    }
+	@Transactional
+	public Tenant createTenant(Tenant tenant) {
+		getEntityManager().persist(tenant);
+		return tenant;
+	}
 
-    @Override
-    @Transactional
-    public void removeTenantById(Long id){
-        getEntityManager().remove(getTenantById(id));
-    }
+	@Override
+	@Transactional
+	public void removeTenantById(Long id) {
+		getEntityManager().remove(getTenantById(id));
+	}
 
-    @Override
-    @Transactional
-    public Tenant merge(Tenant tenant){
-        return getEntityManager().merge(tenant);
-    }
-    
-    
-    @Autowired AmqpAdmin admin;
-    @Autowired ConnectionFactory connectionFactory;
-    @Autowired StockCompanyService stockCompanyService;
-    @Autowired StockQuoteService stockQuoteService;
-    
-    @Override
-    public void updateQueueBindings(Tenant tenant) {   	    	
-    	String queueName = tenant.getName();
-    	admin.deleteQueue(queueName);
-    	Queue queue = new Queue(queueName);
-    	admin.declareQueue(queue);
-    	
-    	DirectExchange exchange = new DirectExchange(EXCHANGE_NAME);
-    	admin.declareExchange(exchange);
-    
-    	
-    	SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-    	container.setConnectionFactory(connectionFactory);
-    	container.setQueueNames(queueName);
-    	container.setMessageListener(/*new MessageListenerAdapter(*/
-    			new StockQuoteListener(tenant.getName(), this, stockCompanyService, stockQuoteService));
+	@Override
+	@Transactional
+	public Tenant merge(Tenant tenant) {
+		return getEntityManager().merge(tenant);
+	}
 
-    	
-    	for(StockCompany company : tenant.getObservedStockCompanies()) {
-    		String routingKey = company.getSymbol();
-    		Binding binding = BindingBuilder.bind(queue).to(exchange).with(routingKey);
-    		admin.declareBinding(binding);
-//    		System.out.println("NEW BINDING: " + binding);
-    	}
-    	
-    	AmqpTemplate template = new RabbitTemplate(connectionFactory);
-    	template.convertAndSend(queueName, "2342#GOOG#dupa");
-    }
-    
-    public void unsubscribe(String queuename, String topic) throws IOException
-    {
-       Connection connection = connectionFactory.createConnection();
-       Channel channel = connection.createChannel(true);
-       channel.exchangeDeclarePassive(EXCHANGE_NAME);
-       channel.queueUnbind(queuename, EXCHANGE_NAME, topic);
-    }
+	@Autowired
+	AmqpAdmin admin;
+	@Autowired
+	ConnectionFactory connectionFactory;
+	@Autowired
+	StockCompanyService stockCompanyService;
+	@Autowired
+	StockQuoteService stockQuoteService;
+
+	@Override
+	public void updateQueueBindings(Tenant tenant) {
+		String queueName = tenant.getName();
+		admin.deleteQueue(queueName);
+		Queue queue = new Queue(queueName);
+		admin.declareQueue(queue);
+
+		DirectExchange exchange = new DirectExchange(EXCHANGE_NAME);
+		admin.declareExchange(exchange);
+
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(new StockQuoteListener(tenant.getName(),
+				this, stockCompanyService, stockQuoteService));
+
+		for (StockCompany company : tenant.getObservedStockCompanies()) {
+			String routingKey = company.getSymbol();
+			Binding binding = BindingBuilder.bind(queue).to(exchange)
+					.with(routingKey);
+			admin.declareBinding(binding);
+		}
+		container.start();
+	}
+
 }
